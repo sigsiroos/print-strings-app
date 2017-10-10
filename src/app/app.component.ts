c2c42ea52f0a73631f2ab9f5b0dba35251cb66c6
sourysanis <syrous.sina@gmail.com>
2017-10-10 07:48:14 +0200

    Print strings
    Print strings commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  words: string[] = new Array();
+  word: string;
+  boxwidth: number;
+  tempwords: string[] = new Array();
+  rerun: boolean;
+
+  constructor() {
+    this.words = [
+      'Windstorm','Bombasto','Magneta','Tornado', 
+      'Windstorm','Bombasto','Magneta','Tornado',
+      'Windstorm','Bombasto','Magneta','Tornado',
+      'Windstorm','Bombasto','Magneta','Tornado'
+    ];
+    this.boxwidth = 100;
+    this.rerun = true;
+    this.word = '';
+  }
+
+  
+  onCountWidth(word: string, i: number) {
+      this.boxwidth -= word.length
+      if(!this.rerun){
+        this.tempwords = [];
+        this.rerun = true;
+      }
+      if(this.boxwidth > 0){
+        this.tempwords[i] = word;
+        return;
+      } else if(this.boxwidth === 0 || this.boxwidth < 0) {
+        this.tempwords[i] = word;
+        this.boxwidth = 100;
+        this.rerun = false;
+        return this.tempwords.join(" ");
+      }
+  }
+
 }
